e01c76e920e5431daa4a4577e10b0cc4506c42f8
VinceCabs <vctcbns@gmail.com>
2020-10-26 23:28:58 +0100

    add unit tests for HAR files

--- tests/test_GAUnit.py
+++ tests/test_GAUnit.py
@@ -7,7 +7,7 @@ class test_GAUnit(unittest.TestCase):
 
         self.g = GAUnit(config_file="config.json")
 
-    def test_check_tracking_from_file_ok(self):
+    def test_check_tracking_from_log_file_ok(self):
         test_case = "home_engie"
         log_file = "tests/home_engie.log"
         checklist = self.g.check_tracking_from_file(
@@ -16,7 +16,7 @@ class test_GAUnit(unittest.TestCase):
         # all hits are here
         self.assertEqual([True, True, True], checklist)
 
-    def test_check_tracking_from_file_hit_missing(self):
+    def test_check_tracking_from_log_file_hit_missing(self):
         test_case = "home_engie"
         log_file = "tests/home_engie_first_missing.log"
         checklist = self.g.check_tracking_from_file(
@@ -24,6 +24,13 @@ class test_GAUnit(unittest.TestCase):
         )
         # first hit is missing
         self.assertEqual([False, True, True], checklist)
+
+    def test_check_tracking_from_har_file_ok(self):
+        test_case = "home_engie"
+        log_file = "tests/home_engie.har"
+        checklist = self.g.check_tracking_from_file(test_case, log_file)
+        # all hits are here
+        self.assertEqual([True, True, True], checklist)
 
 
 if __name__ == "__main__":
