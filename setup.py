838f3968c0cff25101e460b32a3fa5eb19761792
VinceCabs <vctcbns@gmail.com>
2021-01-07 22:34:23 +0100

    require Python 3.7+ (setup.py)

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setuptools.setup(
         ]
     },
     license="MIT",
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=["colorama==0.4.4", "gspread==3.6.0"],
     classifiers=[
         "Development Status :: 3 - Alpha",
