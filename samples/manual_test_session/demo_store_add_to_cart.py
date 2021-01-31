fcbc6862ccf53b089884afd6dc8be467e5b98ae0
VinceCabs <vctcbns@gmail.com>
2021-01-31 14:59:49 +0100

    feat(example): add manual browsing session sample
    It is useful to launch a browsing session with proxy and GAUnit already
    set up.
    This example does that

---
+++ samples/manual_test_session/demo_store_add_to_cart.py
@@ -0,0 +1,75 @@
+""""
+Open a manual test session
+
+Code sample: opens a new browser session and records a har. At the end of the session, 
+it checks the har against tracking plan. Tracking plan is the "add to cart" scenario on 
+Google online store demo described in GAUnit Documentation: 
+https://gaunit.readthedocs.io/en/latest/tutorial.html
+"""
+import json
+from os.path import abspath, dirname, join
+from time import sleep
+from tkinter import messagebox
+
+
+import gaunit
+from browsermobproxy import Server
+from selenium import webdriver
+
+# from selenium.common import Alert
+
+
+def run():
+
+    # set up proxy
+    server = Server()  # or add path to binary: 'Server(path="browsermob-proxy")'
+    server.start()
+    # 'useEcc' is needed to have decent response time with HTTPS
+    proxy = server.create_proxy({"useEcc": True})
+
+    # set up Geckodriver/Firefox
+    # # uncomment if you want to use Firefox
+    # profile = webdriver.FirefoxProfile()
+    # profile.set_proxy(proxy.selenium_proxy())
+    # driver = webdriver.Firefox(firefox_profile=profile)
+
+    # set up Chrome driver
+    options = webdriver.ChromeOptions()
+    options.add_argument("--proxy-server=%s" % proxy.proxy)
+    # options.add_argument("--headless")  # uncomment if you want headless Chrome
+    capabilities = webdriver.DesiredCapabilities.CHROME.copy()
+    capabilities["acceptInsecureCerts"] = True
+    driver = webdriver.Chrome(chrome_options=options, desired_capabilities=capabilities)
+
+    # start test case
+    driver.implicitly_wait(10)
+    test_case = "demo_store_add_to_cart"
+    proxy.new_har(test_case)
+    driver.get("https://enhancedecommerce.appspot.com/")
+
+    messagebox.showinfo(
+        "Manual browsing mode",
+        "Recording network trafic. Browse site and press 'OK' when finished",
+    )
+
+    # export har and close all
+    har = proxy.har
+    server.stop()
+    driver.quit()
+
+    # uncomment if you need to export the har
+    # with open(
+    #     join(abspath(dirname(__file__)), test_case), "w", encoding="utf8"
+    # ) as f:
+    #     json.dump(har, f)
+
+    # check hits against tracking plan and print results
+    path = join(abspath(dirname(__file__)), "tracking_plan.json")
+    tracking_plan = gaunit.TrackingPlan.from_json(path)
+    r = gaunit.check_har(test_case, tracking_plan, har=har)
+
+    r.print_result(display_ok=True)
+
+
+if __name__ == "__main__":
+    run()
